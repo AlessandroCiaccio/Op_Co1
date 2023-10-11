fcb3c74c658a1b1f90f3916b2e8092ee09bd44c4
Alessandro <sandroscro@libero.it>
2023-10-11 19:15:00 +0200

    Added Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int x=2, y=2;
+        boolean z=(x==y);
+        System.out.println("The numbers are "+x+" and "+y);
+        System.out.println("They are equal? "+z);
+    }
+}
